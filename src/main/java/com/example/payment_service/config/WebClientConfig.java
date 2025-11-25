1a3d92e7fea382937e028d4b86098377346edece
201912025 <michi2012@naver.com>
2025-11-25 13:02:05 +0900

    feat: webclient 커넥트 타임아웃 추가

--- src/main/java/com/example/payment_service/config/WebClientConfig.java
+++ src/main/java/com/example/payment_service/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.payment_service.config;
 
+import io.netty.channel.ChannelOption;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,8 @@ public class WebClientConfig {
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         HttpClient httpClient = HttpClient.create()
-                                          .responseTimeout(Duration.ofSeconds(5));
+                                          .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
+                                          .responseTimeout(Duration.ofSeconds(10));
 
         return WebClient.builder()
                         .clientConnector(new ReactorClientHttpConnector(httpClient));
